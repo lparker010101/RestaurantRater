34adfae0d2cbf33e5992d7a20cd082b638ff919b
Laura Parker <laura.parker03@yahoo.com>
2021-10-07 21:18:47 -0400

    Create added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -11,9 +11,30 @@ namespace RestaurantRater.Controllers
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
         // GET: Restaurant / Index
-        public ActionResult Index()
+        public ActionResult Index() // To display all restaurants that exist in a table format.
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant); //_db is not the actual database.  It is a snapshot, so we need to take the changes made and save them.
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
